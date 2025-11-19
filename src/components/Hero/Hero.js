a5bace14ff1d5722ddca03ecac9747137df08bd3
unknown <zdravkodelic1993@gmail.com>
2025-11-19 17:09:41 +0100

    feat: receive 'isInLightbox' prop from App.js and pass it down to HeroSlide.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,10 +2,14 @@ import classes from "./Hero.module.css";
 
 import HeroSlide from "./HeroSlide.js";
 
-export default function Hero({ images }) {
+export default function Hero({ images, isInLightbox }) {
   return (
     <section aria-label="Product image carousel">
-      <HeroSlide classNames={classes} images={images} />
+      <HeroSlide
+        classNames={classes}
+        images={images}
+        isInLightbox={isInLightbox}
+      />
     </section>
   );
 }
