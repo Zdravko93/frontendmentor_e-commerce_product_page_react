53f45898bef6b8e4aecc02d8dc04b53e1f8c30da
unknown <zdravkodelic1993@gmail.com>
2025-11-19 01:53:11 +0100

    accessibility: add aria-hiden = 'true' so screen readers ignore the element

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,7 +1,12 @@
 import classes from "./Backdrop.module.css";
 
 export default function Backdrop({ show, onClick }) {
+  /* Backdrop is purely visual, screen readers should ignore it */
   return show ? (
-    <div className={classes.backdrop} onClick={onClick}></div>
+    <div
+      aria-hidden="true"
+      className={classes.backdrop}
+      onClick={onClick}
+    ></div>
   ) : null;
 }
