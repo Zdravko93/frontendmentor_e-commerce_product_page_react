13f01f4b5c442af1ba8321fd6a38d74a5f31cb80
unknown <zdravkodelic1993@gmail.com>
2025-11-19 22:47:57 +0100

    accessibility: wrap pricing content with <div>/role='group'/aria-label='Pricing information

--- src/components/SneakerDetails/SneakerDetailsPrices.js
+++ src/components/SneakerDetails/SneakerDetailsPrices.js
@@ -3,12 +3,17 @@ import Card from "../Card/Card.js";
 export default function SneakerDetailsPrices({ classNames }) {
   return (
     <Card>
-      <div className={classNames["new-price"]}>
-        <span className={classNames["current-price"]}>$125.00</span>
-        <span className={classNames.discount}>50%</span>
-      </div>
-      <div className={classNames["old-price"]}>
-        <span>$250.00</span>
+      <div role="group" aria-label="Pricing information">
+        <div
+          className={classNames["new-price"]}
+          aria-label="Current price $125, 50% off"
+        >
+          <span className={classNames["current-price"]}>$125.00</span>
+          <span className={classNames.discount}>50%</span>
+        </div>
+        <div className={classNames["old-price"]}>
+          <span aria-label="original-price $250">$250.00</span>
+        </div>
       </div>
     </Card>
   );
