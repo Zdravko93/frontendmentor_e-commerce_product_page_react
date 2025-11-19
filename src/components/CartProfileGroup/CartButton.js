59904baefb801bacfe28befbd3db3f3873db88d4
unknown <zdravkodelic1993@gmail.com>
2025-11-20 00:26:07 +0100

    accessibility: add 'aria-expanded', 'aria-controls' and 'aria-hidden

--- src/components/CartProfileGroup/CartButton.js
+++ src/components/CartProfileGroup/CartButton.js
@@ -2,9 +2,15 @@ import cartImg from "../../assets/icon-cart.svg";
 
 import classes from "./CartButton.module.css";
 
-export default function CartButton({ onCartToggle }) {
+export default function CartButton({ onCartToggle, isCartOpen }) {
   return (
-    <button className={classes["cart-btn"]} onClick={onCartToggle}>
+    <button
+      className={classes["cart-btn"]}
+      onClick={onCartToggle}
+      aria-expanded={isCartOpen}
+      aria-controls="cart-panel"
+      aria-hidden={isCartOpen ? "false" : "true"}
+    >
       <img src={cartImg} alt="Shopping cart" />
     </button>
   );
