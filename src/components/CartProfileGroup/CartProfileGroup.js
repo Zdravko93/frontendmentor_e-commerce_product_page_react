d41a3618b7bd52ddef4e302ad29863bbad77cd31
unknown <zdravkodelic1993@gmail.com>
2025-11-20 00:27:18 +0100

    refactor: receive isCartOpen prop and pass it down to CartButton

--- src/components/CartProfileGroup/CartProfileGroup.js
+++ src/components/CartProfileGroup/CartProfileGroup.js
@@ -3,11 +3,15 @@ import classes from "./CartProfileGroup.module.css";
 import CartButton from "./CartButton.js";
 import UserProfileButton from "./UserProfileButton.js";
 
-export default function CartProfileGroup({ onCartToggle, totalCartItems }) {
+export default function CartProfileGroup({
+  onCartToggle,
+  totalCartItems,
+  isCartOpen,
+}) {
   return (
     <div className={classes["cart-profile-group"]}>
       <div className={classes["cart-container"]}>
-        <CartButton onCartToggle={onCartToggle} />
+        <CartButton onCartToggle={onCartToggle} isCartOpen={isCartOpen} />
         {totalCartItems > 0 && (
           <span className={classes["cart-items-number"]}>{totalCartItems}</span>
         )}
