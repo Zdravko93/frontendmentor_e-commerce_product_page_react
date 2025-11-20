eb742f6f63e013f4f19bba078d2864de086f5806
unknown <zdravkodelic1993@gmail.com>
2025-11-20 23:31:54 +0100

    accessibility: remove <button> wrapper inside UserProfileButton.js and its styles, since it has no interactivity yet

--- src/components/CartProfileGroup/UserProfileButton.js
+++ src/components/CartProfileGroup/UserProfileButton.js
@@ -3,13 +3,13 @@ import userImg from "../../assets/image-avatar.png";
 import classes from "./UserProfileButton.module.css";
 
 export default function UserProfileButton() {
+  // Here I previously had <button> as a wrapper element
+  // but currently there is no need for that since avatar doesn't have any functionality yet (Log In/Log Out, Register, Action Menu etc)
   return (
-    <button className={classes["user-profile-button"]}>
-      <img
-        className={classes["user-profile-img"]}
-        src={userImg}
-        alt="User profile"
-      />
-    </button>
+    <img
+      className={classes["user-profile-img"]}
+      src={userImg}
+      alt="User profile"
+    />
   );
 }
