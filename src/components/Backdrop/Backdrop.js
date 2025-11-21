2c42c03c8badbbae260a861f9d2dd374e74544c5
unknown <zdravkodelic1993@gmail.com>
2025-11-21 01:06:00 +0100

    accessibility: remove previously passed onClick prop; add tabIndex={-1}

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,12 +1,8 @@
 import classes from "./Backdrop.module.css";
 
-export default function Backdrop({ show, onClick }) {
+export default function Backdrop({ show }) {
   /* Backdrop is purely visual, screen readers should ignore it */
   return show ? (
-    <div
-      aria-hidden="true"
-      className={classes.backdrop}
-      onClick={onClick}
-    ></div>
+    <div aria-hidden="true" className={classes.backdrop} tabIndex={-1}></div>
   ) : null;
 }
