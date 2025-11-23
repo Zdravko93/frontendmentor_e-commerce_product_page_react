1e0381dcdbecebd101acf14e7a62d7f48ddfe6fa
unknown <zdravkodelic1993@gmail.com>
2025-11-23 13:02:17 +0100

    chore: replace React.Fragment with <ul> for better semantics and readability

--- src/components/MenuLinks/MenuLinks.js
+++ src/components/MenuLinks/MenuLinks.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import classesDesktopMenu from "../DesktopHeader/DesktopMenu.module.css";
 import classesMobileMenu from "../MobileHeader/MobileMenu.module.css";
 
@@ -9,23 +7,21 @@ const links = ["Collections", "Men", "Women", "About", "Contact"];
 
 export default function MenuLinks({ isDesktopSize, isMenuOpen }) {
   return (
-    <nav aria-label={`${isDesktopSize ? "Main menu" : "Mobile menu"}`}>
-      <ul
-        className={
-          isDesktopSize
-            ? classesDesktopMenu["desktop-menu"]
-            : `${classesMobileMenu["mobile-menu"]} ${
-                isMenuOpen ? classesMobileMenu.open : ""
-              }`
-        }
-        aria-hidden={!isMenuOpen && !isDesktopSize}
-      >
-        {links.map((link, index) => (
-          <React.Fragment key={index}>
-            <MenuLink link={link} />
-          </React.Fragment>
-        ))}
-      </ul>
-    </nav>
+    <ul
+      className={
+        isDesktopSize
+          ? classesDesktopMenu["desktop-menu"]
+          : `${classesMobileMenu["mobile-menu"]} ${
+              isMenuOpen ? classesMobileMenu.open : ""
+            }`
+      }
+      aria-hidden={!isMenuOpen && !isDesktopSize}
+    >
+      {links.map((link, index) => (
+        <ul>
+          <MenuLink link={link} key={index} />
+        </ul>
+      ))}
+    </ul>
   );
 }
