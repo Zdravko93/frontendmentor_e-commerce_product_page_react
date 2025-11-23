f465f07d702a4be7250910ac6d8dc7d519f90c58
unknown <zdravkodelic1993@gmail.com>
2025-11-23 22:13:33 +0100

    perf: add React.mamo in Backdrop

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -1,8 +1,10 @@
+import React from "react";
+
 import classes from "./Backdrop.module.css";
 
-export default function Backdrop({ show }) {
+export default React.memo(function Backdrop({ show }) {
   /* Backdrop is purely visual, screen readers should ignore it */
   return show ? (
     <div aria-hidden="true" className={classes.backdrop} tabIndex={-1}></div>
   ) : null;
-}
+});
