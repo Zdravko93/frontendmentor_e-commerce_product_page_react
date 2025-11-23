5205a6393be35d3806e554a50ac6ca9d4a61ff3c
unknown <zdravkodelic1993@gmail.com>
2025-11-23 23:50:07 +0100

    SEO: wrap App with HelmetProvider to enable SEO optimization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+
 import "./index.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
