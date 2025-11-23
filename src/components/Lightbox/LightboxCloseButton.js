951f624ff7ea331881cac4fade30985763f8c07d
unknown <zdravkodelic1993@gmail.com>
2025-11-23 23:14:34 +0100

    perf: add React.memo to LightboxCloseButton to improve performance

--- src/components/Lightbox/LightboxCloseButton.js
+++ src/components/Lightbox/LightboxCloseButton.js
@@ -1,23 +1,22 @@
-import { forwardRef } from "react";
+import React, { forwardRef } from "react";
 
 import classes from "./LightboxCloseButton.module.css";
 
 import LightboxCloseIcon from "./LightboxCloseIcon.js";
 
-const LightboxCloseButton = forwardRef(function LightboxCloseButton(
-  { onLightboxClose },
-  ref
-) {
-  return (
-    <button
-      ref={ref}
-      className={classes["lightbox-close-btn"]}
-      onClick={onLightboxClose}
-      aria-label="Close lightbox"
-    >
-      <LightboxCloseIcon />
-    </button>
-  );
-});
+const LightboxCloseButton = React.memo(
+  forwardRef(function LightboxCloseButton({ onLightboxClose }, ref) {
+    return (
+      <button
+        ref={ref}
+        className={classes["lightbox-close-btn"]}
+        onClick={onLightboxClose}
+        aria-label="Close lightbox"
+      >
+        <LightboxCloseIcon />
+      </button>
+    );
+  })
+);
 
 export default LightboxCloseButton;
